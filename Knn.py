134c2da1b1480a2874bb9431aa452d99136acde1
V2V2V2-droid <74412016+V2V2V2-droid@users.noreply.github.com>
2022-09-01 16:39:47 +0100

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -15,7 +15,6 @@ def standardized_matrix(x):
 
 
 
-#y = np.array(y == "good").astype(int)
 
 # test array
 
@@ -67,66 +66,4 @@ class Knn:
         # 1d arrays for both
         return sum(np.array(y_pred == y_test).astype(int))/len(y_test)
 
-if __name__ == '__main__':
-    wine = pd.read_csv("wine.csv")
-    x = np.array(wine)[:, :-1]
-    y = np.array(wine)[:, -1]
 
-    #we obtain better results without PCA
-
-    # let's try variable selection from the pairplot
-    # remove residual sugar, chloride and fixed acidity
-    x = x[:, [0,1, 2, 5, 6, 9, 10]]
-
-
-    # PCA
-    #pca = Pca(x, d=6)
-    #r_proj = pca.reduced_x
-    #f = lambda u: u.real 
-    #x = f(r_proj)
-
-    # standardize matrix x
-    x = standardized_matrix(x)
-
-    # train test split:
-    x_train = x[:1550, :]
-    y_train = y[:1550]
-    x_test = x[1550:, :]
-    y_test = y[1550:]
-
-
-    # choose right k: even if that would need CV
-    score = []
-    for k in range(1, 15):
-        print("k={}".format(k))
-        A = Knn(x=x_train, k=k, y=y_train)
-        y_pred = A.predict(datapoints=x_test)
-        score.append(A.score(y_pred=y_pred, y_test=y_test))
-    print(score)
-
-    best_k = np.argmax(score)
-    print(best_k)
-
-# arrives at about 75 score max: even if we should refine k using cross validation
-
-
-    # plot true data
-
-    #df = pd.DataFrame(r_proj)
-    #df["quality"] = wine["quality"]
-    #sn.pairplot(df, hue="quality")
-    #plt.show()
-
-    # plot the y_test true
-
-    #df1 = pd.DataFrame(x_test)
-    #df1["quality"] = y_test
-    #sn.pairplot(df1, hue="quality")
-    #plt.show()
-
-    # plot out y_pred
-
-    #df2 = pd.DataFrame(x_test)
-    #df2["quality"] = y_pred
-    #sn.pairplot(df2, hue="quality")
-    #plt.show()
